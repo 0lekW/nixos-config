8e0dee7013ad9fae93046cd35e955b41386dd08a
olek <alexjwardega@gmail.com>
2025-08-07 21:08:21 +1200

    Initial flake based homelab config

---
+++ flake.nix
@@ -0,0 +1,17 @@
+{
+  description = "Flake-based NixOS config for homelab";
+
+  inputs = {
+    nixpkgs.url = "github:NixOS/nixpkgs/nixos-25.05";
+    flake-utils.url = "github:numtide/flake-utils";
+  };
+
+  outputs = { self, nixpkgs, flake-utils, ... }: {
+    nixosConfigurations.homelab = nixpkgs.lib.nixosSystem {
+      system = "x86_64-linux";
+      modules = [
+        ./hosts/homelab/configuration.nix
+      ];
+    };
+  };
+}
