83e981c5996812bc87be4504d8f329c88f022c56
olek_homelab <alexjwardega@gmail.com>
2025-09-23 12:17:34 +1200

    Added nodejs and made change so that vscode remote ssh would work

--- hosts/homelab/configuration.nix
+++ hosts/homelab/configuration.nix
@@ -73,7 +73,10 @@
   vim # Do not forget to add an editor to edit configuration.nix! The Nano editor is also installed by default.
   git
   lm_sensors
+  nodejs
   ];
+
+  programs.nix-ld.enable = true;
 
   # Some programs need SUID wrappers, can be configured further or are
   # started in user sessions.
